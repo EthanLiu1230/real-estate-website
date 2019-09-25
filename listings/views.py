ffce23d922b21b20e53cd9da26e8ed4651aae216
Ethan Liu <liuyuxuan1230@outlook.com>
2019-09-25 16:05:15 +0800

    Display Listings in template. Humanize price and time.

--- listings/views.py
+++ listings/views.py
@@ -6,7 +6,7 @@ def index(request):
     return render(request, 'listings/listings.html')
 
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 
