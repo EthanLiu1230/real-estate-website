dc2819f1ab75ccbf77a596788fc160b50238ba54
Ethan Liu <liuyuxuan1230@outlook.com>
2019-09-25 20:03:56 +0800

    On Listing App: Pagination. Descendent order. Filter by 'is_published'.

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,25 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+from .models import Listing
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'listings/listings.html')
+    # Get all listing
+    # '-' for descendent
+    # listings = Listing.object.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    # Pagination
+    paginator = Paginator(listings, 1)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings,
+    }
+
+    return render(request, 'listings/listings.html', context)
 
 
 def listing(request, listing_id):
